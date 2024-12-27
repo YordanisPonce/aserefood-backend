3b9174a40ef9d9085196b0dc236728826c302ca6
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2024-12-27 00:32:45 -0500

    feat: filt, ord and pag in orders me and new order/me/:id

---
+++ src/orders/dto/out/order-me.out.dto.ts
@@ -0,0 +1,57 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { OrderStatus } from '../../../database/entities/constants';
+import { PaymentSelection } from '../in/order.in.dto';
+import ProductOutDto from '../../../products/dto/out/product.out.dto';
+import ProductComboOutDto from '../../../product-combos/dto/out/product-combo.out.dto';
+
+export class OrderItemMeOutDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty({nullable: true})
+  product?: ProductOutDto;
+
+  @ApiProperty({nullable: true})
+  productCombo?: ProductComboOutDto;
+
+  @ApiProperty()
+  amount: number;
+}
+
+export default class OrderMeOutDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  code: string;
+
+  @ApiProperty()
+  municipalityId: number;
+
+  @ApiProperty()
+  municipalityName: string;
+
+  @ApiProperty()
+  contactInfoId: number;
+
+  @ApiProperty({ enum: OrderStatus })
+  status: OrderStatus;
+
+  @ApiProperty()
+  createdDate: Date;
+
+  @ApiProperty()
+  updatedDate: Date;
+
+  @ApiProperty({ enum: PaymentSelection })
+  paymentSelection: PaymentSelection;
+
+  @ApiProperty()
+  totalAmount: number;
+
+  @ApiProperty()
+  deliveryMethodId: number;
+
+  @ApiProperty({ type: [OrderItemMeOutDto] })
+  orderItems: OrderItemMeOutDto[];
+}
