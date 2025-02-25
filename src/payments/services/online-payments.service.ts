37d2f3ade53536db3ca233d4d3f47384ac92c8e2
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2025-02-24 21:08:19 -0500

    fix: online payment fixture

--- src/payments/services/online-payments.service.ts
+++ src/payments/services/online-payments.service.ts
@@ -1,9 +1,14 @@
-import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import PgService from '../../database/services/pg.service';
-import MailService from '../../mail/services/mail.service';
 import OnlinePayment from '../../database/entities/online-payment.entity';
 import OnlinePaymentOutDto from '../dto/out/online-payment.out.dto';
 import MinioService from '../../minio/services/minio.service';
+import { Role } from '../../auth/decorators/roles.decorator';
 
 @Injectable()
 export default class OnlinePaymentsService {
@@ -14,23 +19,40 @@ export default class OnlinePaymentsService {
     private readonly minioService: MinioService,
   ) {}
 
-  async getPaymentByOrderId(orderId: number): Promise<OnlinePaymentOutDto> {
-    const order = await this.pgService.orders.findOne({where: {id: orderId}});
-    if(!order) {
+  async getPaymentByOrderId(
+    orderId: number,
+    userId: number,
+  ): Promise<OnlinePaymentOutDto> {
+    const order = await this.pgService.orders.findOne({
+      where: { id: orderId },
+    });
+    if (!order) {
       throw new NotFoundException(`Order with id ${orderId} not found`);
     }
-    if(!order.onlinePaymentId){
-      throw new BadRequestException(`Order with id ${orderId} has no online payment`);
+    if (!order.onlinePaymentId) {
+      throw new BadRequestException(
+        `Order with id ${orderId} has no online payment`,
+      );
     }
 
-    return this.getPaymentById(order.onlinePaymentId);
+    return this.getPaymentById(order.onlinePaymentId, userId);
   }
 
-  async getPaymentById(id: number): Promise<OnlinePaymentOutDto>{
-    const payment = await this.pgService.onlinePayments.findOne({where: {id: id}});
-    if(!payment) {
-      throw new NotFoundException(`Online Payment with id ${id} not found`);
+  async getPaymentById(
+    id: number,
+    userId: number,
+  ): Promise<OnlinePaymentOutDto> {
+    const user = await this.pgService.users.findOne({ where: { id: userId } });
+    const payment = await this.pgService.onlinePayments.findOne({
+      where: { id: id },
+      relations: ['order'],
+    });
+    if (user.role === Role.Customer && payment.order.userId !== userId) {
+      throw new NotFoundException(`Online Payment with id ${id} not found for current user`)
     }
+      if (!payment) {
+        throw new NotFoundException(`Online Payment with id ${id} not found`);
+      }
 
     return this.mapToDto(payment);
   }
@@ -50,7 +72,9 @@ export default class OnlinePaymentsService {
     dto.phoneNumber = payment.phoneNumber;
     dto.postalCode = payment.postalCode;
     dto.state = payment.state;
-    dto.screenshot =  payment.screenshot ? (await this.minioService.getPresignedUrl(payment.screenshot)) : null;
+    dto.screenshot = payment.screenshot
+      ? await this.minioService.getPresignedUrl(payment.screenshot)
+      : null;
     return dto;
   }
 }
